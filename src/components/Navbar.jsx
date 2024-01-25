0b9d0ac996d8bf511dcc5339746fcc7086e65c37
Martin <martinmmbote@gmail.com>
2024-01-25 14:41:36 +0300

    navlink

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,23 @@
-import React from 'react'
+import { NavLink } from "react-router-dom"
+
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <header className="header">
+      <NavLink to={"/"} className="w-10 h-10 rounded-lg bg-white items-center justify-center flex font-bold shadow-md">
+        <p className="blue-gradient_text">MM</p>
+      </NavLink>
+
+      <nav className="flex text-lg gap-7 font-medium">
+        <NavLink to="/about" className={({ isActive }) => isActive ? 'text-blue-500' : 'text-black'}>   {/* function to change color   of text when pressed */}
+          About
+        </NavLink>
+
+        <NavLink to="/projects" className={({ isActive }) => isActive ? 'text-blue-500' : 'text-black'}>   {/* function to change color   of text when pressed */}
+          Projects
+        </NavLink>
+      </nav>
+    </header>
   )
 }
 
